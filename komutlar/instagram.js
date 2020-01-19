f2b5dca9e05237734fc222c0c5a3b1639dfba0fe
Glitch (woxe-eco-music) <none>
2020-01-19 17:07:00 +0000

    🏆💶 Checkpoint ./server.js:5804863/3111 ./package.json:5804863/2178 ./util/eventLoader.js:5804863/554 ./events/message.js:5804863/687 ./ayarlar.json:5804863/60 ./komutlar-ekonomi-bilgiler.js:5804863/1901 ./komutlar/ekonomi-cüzdan.js:5804863/2749 ./komutlar/ekonomi-günlük.js:5804863/9254 ./komutlar/ekonomi-hesap-oluş.js:5804863/10539 ./komutlar/ekonomi-hesap-hesapsil.js:5804863/7140 ./komutlar/ekonomi-kasaaç.js:5804863/5669 ./komutlar/ekonomi-kasabilgi.js:5804863/8020 ./komutlar/ekonomi-kasalar.js:5804863/3908 ./komutlar/ekonomi-paraekle.js:5804863/2805 ./komutlar/ekonomi-transfer.js:5804863/5026 ./komutlar/instagram.js:5804863/3068 ./ayrıl.js:5804863/7103 ./kasalar.json:5804863/629

---
+++ komutlar/instagram.js
@@ -0,0 +1,96 @@
+const Discord = require("discord.js");
+const instagram = require("user-instagram");
+const Canvas = require("canvas"),
+  Image = Canvas.Image,
+  Font = Canvas.Font,
+  path = require("path");
+const request = require("node-superfetch");
+
+exports.run = async (client, message, args) => {
+  let kullanici = args.join(" ");
+  if (!kullanici) return message.reply(`Bir kullanıcı adı girmediniz.`);
+
+  instagram("https://www.instagram.com/" + kullanici).then(async data => {
+    const biocuk = data.bio.length === 0 ? "Yok" : data.bio;
+    const isimcik = data.fullName.length === 0 ? "Yok" : data.fullName;
+
+    var gizlimi;
+    var onaylimi;
+
+    if (data.isPrivate === false) gizlimi = "Hayır";
+    if (data.isPrivate === true) gizlimi = "Evet";
+    if (data.isVerified === false) onaylimi = "Hayır";
+    if (data.isVerified === true) onaylimi = "Evet";
+
+    const { registerFont, createCanvas, loadImage } = require("canvas");
+    const canvas = Canvas.createCanvas(401, 202);
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    
+    const avatar = await Canvas.loadImage(`${data.avatarHD}`)
+    ctx.drawImage(avatar , 25 , 25 , 75 , 75)
+    
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText("İsim:" , 115, 40 )
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText(`${isimcik}` , 115, 60 )
+    
+    
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText("Takipçi Sayısı:" , 115, 140 )
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText(`${data.subscriberCount}` , 115, 160 )
+    
+    
+    
+     ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText("Gönderi Sayısı:" , 115, 90 )
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = "BLACK";
+    ctx.fillText(`${data.postCount}` , 115, 110 )
+    
+    //<img src="https://img.icons8.com/flat_round/64/000000/lock--v1.png">
+    //<img src="https://img.icons8.com/flat_round/64/000000/unlock.png">
+    //<img src="https://img.icons8.com/office/16/000000/checked.png">
+    
+    if(gizlimi == "Evet"){
+     const kilitli = await Canvas.loadImage(`https://img.icons8.com/flat_round/64/000000/lock--v1.png`)
+      ctx.drawImage(kilitli , 335 , 15 , 50 , 50)
+    
+    }else{
+      const kilitsiz = await Canvas.loadImage(`https://img.icons8.com/flat_round/64/000000/unlock.png`)
+      ctx.drawImage(kilitsiz , 335 , 15 , 50 , 50)
+    }
+    if(onaylimi == "Evet"){
+      const onay = await Canvas.loadImage(`https://img.icons8.com/office/16/000000/checked.png`)
+      ctx.drawImage(onay, 192 , 47 , 15 , 15)
+    }
+    const attachment = new Discord.Attachment(
+              canvas.toBuffer(),
+              "Hoşgeldin.png"
+            );
+    
+    message.channel.send(attachment)
+    
+    
+
+  });
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "c",
+  description: "Belirlenen instagram hesabı hakkında bilgi verir.",
+  usage: "instagram <hesap ismi>"
+};
--- server.js
+++ server.js
@@ -1,22 +1,94 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require("express");
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+
+const http = require('http');
+    app.get("/", (request, response) => {
+    console.log(`[PING] Açık tutuyorum...`);
+    response.sendStatus(200);
+    });
+    setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    }, 280000);
+const Discord =require('discord.js');
+const client = new Discord.Client();
+const chalk = require('chalk');
+const prefix = "!";
+const fs = require('fs');
+const db = require("quick.db")
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
 });
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
