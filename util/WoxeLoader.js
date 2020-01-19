4871a0ea66662e686c124fb1c0e0421b1ea59f11
Glitch (woxe-eco-music) <none>
2020-01-19 16:57:17 +0000

    Rewound to commit 30930b4ced60957ddaad82ade680e8532821de91 + 0 edits

--- util/WoxeLoader.js
+++
@@ -1,5 +0,0 @@
-const woxe = (event) => require(`../events/${event}`);
-module.exports = client => {
-  client.on('message', woxe('message'));
- };
-//Ekliyeceğiniz Şeylerdeki require kısmını woxe Yapın
