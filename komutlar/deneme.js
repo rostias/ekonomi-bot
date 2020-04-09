52f8c824fcd35e718b4505a6061f197ff3cf382f
Glitch (woxe-eco-music) <none>
2020-04-09 07:17:59 +0000

    💰🏰 Checkpoint ./server.js:3756271/6

--- komutlar/deneme.js
+++
@@ -1,7 +0,0 @@
-client.on('message', msg => {
-  if (msg.content === 'sea') {
-    msg.reply(':tada: Tebrikler! Gizli kodu başarıyla buldun. Rolün başarıyla verilmiştir. :tada:');
-    msg.guild.roles.find(role => role.name === "Lucky");
-    msg.addRole
-  }
-}); 
